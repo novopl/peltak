6be8d484546e68afe0106868cfd345a2c4fc7b48
Mateusz Klos <novopl@gmail.com>
2017-10-26 23:34:07 +0100

    Reimplement config and fabfile based proj_path
    Closes #1 and #2

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,21 @@ This is fabrics configuration file.
 """
 from __future__ import absolute_import
 
+# Configure the build
+from ops.commands.common import conf
+conf.init({
+    'SRC_DIR': 'src',
+    'SRC_PATH': 'src/simplechat',
+    'BUILD_DIR': '.build',
+    'PKGS_PATHS': [
+        'ops/commands',
+    ],
+    'TEST_TYPES': {
+        'default': {'paths': ['ops/commands']}
+    }
+})
+
+
 # Import all commands
 from ops.commands.clean import *
 from ops.commands.docs import *
