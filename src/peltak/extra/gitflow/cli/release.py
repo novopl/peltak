0a7eb246cfd45d6ff05a523ad18b414060cb174b
Mateusz Klos <novopl@gmail.com>
2018-10-07 22:04:24 +0100

    Fix release CLI

--- src/peltak/extra/gitflow/cli/release.py
+++ src/peltak/extra/gitflow/cli/release.py
@@ -3,7 +3,7 @@
 from peltak.cli import cli, click
 
 
-@cli.group('hotfix')
+@cli.group('release', invoke_without_command=True)
 @click.option(
     '-c', '--component',
     type=click.Choice(['major', 'minor', 'patch']),
