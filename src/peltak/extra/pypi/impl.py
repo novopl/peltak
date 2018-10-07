f83c124cce0f8abd36a5165d48baa648d2027815
Mateusz Klos <novopl@gmail.com>
2018-10-07 21:08:19 +0100

    Rename what's left of peltak release to peltak pypi
    Closes #156

--- src/peltak/extra/pypi/impl.py
+++ src/peltak/extra/pypi/impl.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Release commands implementation. """
+""" pypi related commands implementation. """
 from __future__ import absolute_import, unicode_literals
 
 # stdlib imports
