bdcfda1ae061a59e82a61879fd73a2640354bebe
Mateusz Klos <novopl@gmail.com>
2018-11-04 12:57:40 +0000

    Refactor linting implementation
    (feature) Support 3rd party linters configurable through pelconf.
    
    Closes #131

--- src/peltak/commands/lint.py
+++ src/peltak/commands/lint.py
@@ -3,6 +3,8 @@
 from __future__ import absolute_import, unicode_literals
 
 # stdlib imports
+from collections import OrderedDict
+from functools import wraps
 from itertools import chain
 from typing import List
 
@@ -18,86 +20,12 @@ from peltak.core import shell
 from peltak.core import util
 
 
-def _lint_files(paths, include=None, exclude=None, pretend=False):
-    # type: (List[str], List[str], List[str], bool) -> bool
-    """ Run static analysis on the given files.
-
-    Args:
-        paths (list[str]):
-            Iterable with each item being path that should be linted.
-        include (list[str]):
-            A white list of glob patterns. If given, only files that match those
-            globs will be linted (filtered by exclude).
-        exclude (list[str]):
-            A list of glob string patterns to test against. If the file/path
-            matches any of those patters, it will be filtered out.
-        pretend (bool):
-            If set to **True** do not actually run the checks but rather
-            just show the list of files that would be linted.
-
-    Returns:
-        bool: **True** if linting did not return any errors, **False**
-        otherwise.
-    """
-
-    allow_empty = True
-    pylint_cfg_path = conf.get_path('lint.pylint_cfg', 'ops/tools/pylint.ini')
-    pep8_cfg_path = conf.get_path('lint.pep8_cfg', 'ops/tools/pep8.ini')
-
-    if isinstance(paths, string_types):
-        raise ValueError("paths must be an array of strings")
-
-    log.info("Paths to lint:")
-    for path in paths:
-        log.info("  <0>{}", path)
-
-    with util.timed_block() as t:
-        files = list(chain.from_iterable(
-            fs.filtered_walk(p, include, exclude) for p in paths
-        ))
-
-    if files:
-        log.info("Files:")
-        for p in files:
-            log.info("  <0>{}", p)
-    else:
-        log.err("No files found for linting, exiting...")
-        return allow_empty
-
-    log.info("Collected <33>{} <32>files in <33>{}s".format(
-        len(files), t.elapsed_s
-    ))
-
-    if not pretend:
-        log.info("Checking PEP8 compatibility")
-        files = fs.surround_paths_with_quotes(files)
-
-        with util.timed_block() as t:
-            pep8_cmd = 'pep8 --config {} {}'.format(pep8_cfg_path, files)
-            pep8_ret = shell.run(pep8_cmd, exit_on_error=False).return_code
-
-            log.info("Running linter")
-            pylint_cmd = 'pylint --rcfile {} {}'.format(pylint_cfg_path, files)
-            pylint_ret = shell.run(pylint_cmd, exit_on_error=False).return_code
-
-        log.info("Code checked in <33>{}s", t.elapsed_s)
-
-        if pep8_ret != 0:
-            log.info("pep8 failed with return code {}", pep8_ret)
-
-        if pylint_ret:
-            log.info("pylint failed with return code {}", pylint_ret)
-
-        return pep8_ret == pylint_ret == 0
-
-    return True
+g_linters = OrderedDict()
 
 
 def lint(exclude, skip_untracked, commit_only, pretend):
     # type: (List[str], bool, bool, bool) -> None
     """ Lint python files.
-
-    TODO: Introduce a linter interface to allow support for arbitrary tools
 
     Args:
         exclude (list[str]):
@@ -111,21 +39,187 @@ def lint(exclude, skip_untracked, commit_only, pretend):
             If set to **True** do not actually run the checks but rather
             just show the list of files that would be linted.
     """
-    paths = [conf.proj_path(p) for p in conf.get('lint.paths', [])]
-    exclude = list(exclude)     # Convert from tuple to easily concatenate.
-    exclude = exclude or conf.get('lint.exclude', [])
+    runner = LintRunner(exclude, skip_untracked, commit_only, pretend)
 
-    if commit_only:
-        include = ['*' + f for f in git.staged() if f.endswith('.py')]
-        exclude += git.ignore()
-    else:
-        include = ['*.py']
-
-    if skip_untracked:
-        exclude += git.untracked()
-
-    if not _lint_files(paths, include, exclude, pretend):
+    if not runner.run():
         exit(1)
+
+
+def tool(name):
+    global g_linters
+
+    def decorator(fn):
+        g_linters[name] = fn
+        return fn
+
+    return decorator
+
+
+class Linter(object):
+    """ Base class for integrations with 3rd party tools.
+
+    Each linter tool should subclass this class and implement the `run` method.
+
+    Attributes:
+        retcode (Optional[int]):
+            The implementation should store the return code in this attribute
+            for later access. **0** means success, **None** means the tool was
+            not ran yet.
+    """
+    name = '(unnamed linter)'
+
+    def __init__(self):
+        # type: () -> None
+        self.retcode = None
+
+    def check(self, files):
+        # type; (List[str]) -> bool
+        """ Check the given files.
+
+        This is the method that need to be overridden by the subclasses. It
+        should perform it's checks,  store the return code in `retcode`
+        attribute and return a bool indicating if the the code passed the check.
+
+        Args:
+            files (list[str]):
+                The list of files to check.
+
+        Returns:
+            bool: **True** if all files are deemed ok, **False** otherwise.
+        """
+        raise NotImplementedError("Subclasses must implement check() method")
+
+
+class LintRunner(object):
+    """ Lint runner class.
+
+    This class represents a single linter run that includes running all the
+    tools configured. It will notify the user about progress through stdout.
+    """
+    def __init__(self, exclude, skip_untracked, commit_only, pretend):
+        # type: (List[str], bool, bool, bool) -> None
+        """ Run static analysis on the given files.
+
+        Args:
+            exclude (list[str]):
+                A list of glob string patterns to test against. If the file/path
+                matches any of those patters, it will be filtered out.
+            skip_untracked (bool):
+                If set to **True** it will skip all files not tracked by git.
+            commit_only (bool):
+                Only lint files that are staged for commit.
+            pretend (bool):
+                If set to **True** do not actually run the checks but rather
+                just show the list of files that would be linted.
+            linters (list[Linter]):
+                A list of linters to run against the code.
+
+        Returns:
+            bool: **True** if linting did not return any errors, **False**
+            otherwise.
+        """
+        self.exclude = exclude
+        self.skip_untracked = skip_untracked
+        self.commit_only = commit_only
+        self.pretend = pretend
+        self.allow_empty = True
+
+        self.linters = OrderedDict()
+        for tool in conf.get('lint.tools', ['pep8', 'pylint']):
+            if tool in g_linters:
+                self.linters[tool] = g_linters[tool]
+            else:
+                log.err("Unknown lint tool <35>{}", tool)
+
+    def run(self):
+        """ Run all linters and report results.
+
+        Returns:
+            bool: **True** if all checks were successful, **False** otherwise.
+        """
+        # type: () -> bool
+        with util.timed_block() as t:
+            files = self._collect_files()
+
+        # Print collected files
+        if files:
+            log.info("Files:")
+            for p in files:
+                log.info("  <0>{}", p)
+        else:
+            log.err("No files found for linting, exiting...")
+            return self.allow_empty
+
+        log.info("Collected <33>{} <32>files in <33>{}s".format(
+            len(files), t.elapsed_s
+        ))
+
+        success = True
+        if not self.pretend:
+            results = OrderedDict()
+
+            with util.timed_block() as t:
+                for name, check_fn in self.linters.items():
+                    log.info("Running <35>{}".format(name))
+                    results[name] = check_fn(files)
+
+            log.info("Code checked in <33>{}s", t.elapsed_s)
+
+            for name, retcode in results.items():
+                if retcode != 0:
+                    success = False
+                    log.err("<35>{} <31>failed with return code: <33>{}".format(
+                        name, retcode
+                    ))
+
+        return success
+
+    def _collect_files(self):
+        # type: () -> List[str]
+        paths = [conf.proj_path(p) for p in conf.get('lint.paths', [])]
+        exclude = list(self.exclude)
+        exclude = exclude or conf.get('lint.exclude', [])
+
+        # prepare
+        if self.commit_only:
+            include = ['*' + f for f in git.staged() if f.endswith('.py')]
+            exclude += git.ignore()
+        else:
+            include = ['*.py']
+
+        if self.skip_untracked:
+            exclude += git.untracked()
+
+        if isinstance(paths, string_types):
+            raise ValueError("paths must be an array of strings")
+
+        log.info("Paths to lint:")
+        for path in paths:
+            log.info("  <0>{}", path)
+
+        return list(chain.from_iterable(
+            fs.filtered_walk(p, include, exclude) for p in paths
+        ))
+
+
+@tool('pep8')
+def pep8_check(files):
+    # type: (List[str]) -> int
+    files = fs.surround_paths_with_quotes(files)
+    cfg_path = conf.get_path('lint.pep8_cfg', 'ops/tools/pep8.ini')
+    pep8_cmd = 'pep8 --config {} {}'.format(cfg_path, files)
+
+    return shell.run(pep8_cmd, exit_on_error=False).return_code
+
+
+@tool('pylint')
+def pylint_check(files):
+    # type: (List[str]) -> int
+    files = fs.surround_paths_with_quotes(files)
+    cfg_path = conf.get_path('lint.pylint_cfg', 'ops/tools/pylint.ini')
+    pylint_cmd = 'pylint --rcfile {} {}'.format(cfg_path, files)
+
+    return shell.run(pylint_cmd, exit_on_error=False).return_code
 
 
 # Used in docstrings only until we drop python2 support
