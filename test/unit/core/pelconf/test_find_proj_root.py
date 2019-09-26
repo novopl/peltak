248f8fb975de926d6c39cb8a17adb59eb093a03b
Mateusz Klos <novopl@gmail.com>
2019-09-26 14:55:25 +0100

    Fix failing tests

--- test/unit/core/pelconf/test_find_proj_root.py
+++ test/unit/core/pelconf/test_find_proj_root.py
@@ -13,14 +13,20 @@ def test_runs_detection_when_no_global_variable_stored():
     util.cached_result.clear(pelconf._find_proj_root)
     assert pelconf._find_proj_root() == '/fake/proj/root'
 
+    util.cached_result.clear(pelconf._find_proj_root)
+
 
 @testing.patch_proj_root('/fake/proj/root', nest_level=2)
 def test_works_with_multiple_levels_of_nesting():
     util.cached_result.clear(pelconf._find_proj_root)
     assert pelconf._find_proj_root() == '/fake/proj/root'
 
+    util.cached_result.clear(pelconf._find_proj_root)
+
 
 @testing.patch_proj_root(None)
 def test_returns_none_if_not_found():
     util.cached_result.clear(pelconf._find_proj_root)
     assert pelconf._find_proj_root() is None
+
+    util.cached_result.clear(pelconf._find_proj_root)
