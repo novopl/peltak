9c4520daeb813cfbd75a5b925182396eeebabceb
Mateusz Klos <novopl@gmail.com>
2020-01-13 08:51:25 +0000

    Releasing v0.26.8

--- src/peltak/__init__.py
+++ src/peltak/__init__.py
@@ -17,5 +17,5 @@
 from __future__ import absolute_import, unicode_literals
 from os.path import abspath, dirname
 
-__version__ = '0.26.7'
+__version__ = '0.26.8'
 PKG_DIR = abspath(dirname(__file__))
