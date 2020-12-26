1eadf5c24c1f67f99ddc9041799b614d5841a51a
Mateusz Klos <novopl@gmail.com>
2020-12-26 19:32:08 +0000

    Move test_data fixture to testing.fixtures
    We should keep all the fixtures in the codebase, the tests should only import it.
    We need to think what to do with the test data. If someone installs peltak package
    and tried to use test data based fixtures in his own tests then the whole thing will
    collapse.

--- test/unit/conftest.py
+++ test/unit/conftest.py
@@ -1,26 +1,2 @@
-""" pytest setup """
-from os.path import dirname, join
-
-import pytest
-
-from peltak import testing
-
-
-DATA_DIR = join(dirname(dirname(__file__)), 'data')
-
-
-@pytest.fixture()
-def test_data():
-    """ Returns a context manager to work within temporary test project.
-
-    This ensures each test has a clean copy of the data.
-
-    :return testing.TestDataProvider:
-        TestDataProvider instance that can be used to simulate working within
-        one of the test data directories.
-    """
-    return testing.TestDataProvider(DATA_DIR)
-
-
 # Import all fixtures
 from peltak.testing.fixtures import *   # noqa pylint: disable=wildcard-import unused-import unused-wildcard-import
