7dfca1013d7578742586e8c396d40090676b9a10
Mateusz Klos <novopl@gmail.com>
2021-12-16 11:36:25 +0100

    todos: Add CLI command help
    All commands should have a nice help.

--- plugins/peltak-todos/src/peltak_todos/cli.py
+++ plugins/peltak-todos/src/peltak_todos/cli.py
@@ -27,12 +27,27 @@ from peltak.commands import click, root_cli, verbose_option
 #  Should check all files in the given directory.
 @root_cli.command('todos')
 @click.option('-u', '--untracked', is_flag=True, default=False)
-@click.option('-a', '--author', 'authors', type=str, multiple=True, default=None)
-@click.option('--verify-complete', is_flag=True, default=False)
+@click.option(
+    '-a',
+    '--author',
+    'authors',
+    type=str,
+    multiple=True,
+    default=None,
+    help="Show only TODOs for a given author (can use multiple times to show for"
+         "multiple authors)."
+)
+@click.option(
+    '--verify-complete',
+    is_flag=True,
+    default=False,
+    help="Return with non-zero exit code if there are any TODOs left in the code."
+)
 @click.option(
     '-f', '--file', 'file_path',
     type=click.Path(exists=True, dir_okay=False, file_okay=True),
     default=None,
+    help="Check just the given file."
 )
 @verbose_option
 def todos(
@@ -41,6 +56,16 @@ def todos(
     authors: List[str],
     verify_complete: bool,
 ) -> None:
+    """ Scan code for TODOs and print a report.
+
+    Examples::
+
+        \b
+        $ peltak todos
+        $ peltak todos --untracked
+        $ peltak todos --author novopl
+        $ peltak --verify-complete
+    """
     from . import logic
 
     logic.check_todos(untracked, file_path, authors, verify_complete)
